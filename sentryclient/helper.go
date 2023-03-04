2c5a4416f6c91249421f9f6e50a8cf2c94951426
周伟 <wei.zhou@matrixport.com>
2023-03-05 00:37:56 +0800

    biz

--- sentryclient/helper.go
+++ sentryclient/helper.go
@@ -7,22 +7,35 @@ import (
 )
 
 const TraceID = "x_request_id"
+const errorVerbose = "errorVerbose"
 
 type ErrorRecordBase struct {
 	Msg        string `json:"msg" bson:"msg"`
 	XRequestID string `json:"x_request_id" bson:"x_request_id"`
 
-	Detail string `json:"detail" bson:"detail"`
+	Detail       string `json:"detail" bson:"detail"`
+	ErrorVerbose string `json:"error_verbose" bson:"error_verbose"`
 
-	Biz string `json:"biz" bson:"biz"`
+	Biz       string `json:"biz" bson:"biz"`
+	Timestamp int64  `json:"timestamp" bson:"timestamp"`
 }
+
+/*
+业务线： biz
+告警时间： 2023-03-04 14:46:53.288 CST
+告警消息: biz.failed
+告警详情： errorVerbose
+x_request_id： pTriEEmtQBgwInaMQHxyyywyCXhVJhZj
+*/
 
 func FromSentryEvent(e *sentry.Event, biz string) *SendEventReq {
 	return &SendEventReq{
-		Msg:        e.Message,
-		Detail:     gconv.String(e.Extra),
-		XRequestId: gconv.String(e.Extra[TraceID]),
-		Biz:        biz,
+		Msg:          e.Message,
+		Detail:       gconv.String(e.Extra),
+		XRequestId:   gconv.String(e.Extra[TraceID]),
+		ErrorVerbose: gconv.String(e.Extra[errorVerbose]),
+		Biz:          biz,
+		Timestamp:    e.Timestamp.UnixMilli(),
 	}
 }
 
